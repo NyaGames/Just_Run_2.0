aeae44a292b146445a70575d28b40f4f33f32bdd
Jaime <43349828+KaceGameDev@users.noreply.github.com>
2019-01-11 12:12:54 +0100

    Efectos de Sonido
    Efectos de Sonido y menu de opciones

--- Just_Run_2/src/main/resources/static/script/audio.js
+++ Just_Run_2/src/main/resources/static/script/audio.js
@@ -7,6 +7,7 @@ class Audio{
         this.sonidoseguimiento="";
         this.sonidofueramapa="";
         this.captura="";
+        this.andar="";
     }
     //musica de fondo
     loop(){
@@ -15,30 +16,42 @@ class Audio{
     }
     //efecto de saltar
     saltacionar(){
-        
+        this.sonidosalto = game.add.audio('salto.mp3');
+        this.sonidosalto.play();
     }
     //efecto de la trampa rodante (I)
     rodacionar(){
-
+		this.sonidorodante = game.add.audio('BolaNieve.mp3');
+		this.sonidorodante.play();
     }
     //efecto de la trampa de caida (O)
     caidicionar(){
-
+    	this.sonidocaida = game.add.audio('FreeFall.mp3');
+		this.sonidocaida.play();
     }
     //efecto de la trampa lateral o de seguimiento (P)
     seguicionar(){
-
+    	this.sonidoseguimiento = game.add.audio('Deslizar.mp3');
+		this.sonidoseguimiento.play();
     }
     //efecto de salirse fuera del mapa
     salicionar(){
-
+    	this.sonidofueramapa = game.add.audio('salirse.mp3');
+    	this.sonidofueramapa.play();
     }
     //sonido de capturar a otro jugador
     capturacionar(){
+    	this.captura = game.add.audio('capturacionar.mp3');
+    	this.captura.play();
+    }
 
+    andacionar(){
+    	this.andar = game.add.audio('andar.mp3');
+    	this.andar.play();
     }
     
     cambio(){
         this.song.stop();
     }
+
 }
